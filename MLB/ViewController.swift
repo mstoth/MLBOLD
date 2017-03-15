c7d0ca9097a4c798e3e28820022104c9ec51b849
mstoth <michael@virtualpianist.com>
2017-03-15 18:33:47 -0400

    Initial Commit

---
+++ MLB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MLB
+//
+//  Created by Michael Toth on 3/15/17.
+//  Copyright © 2017 Michael Toth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
